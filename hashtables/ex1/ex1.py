04d54c76a339336c7f0e843b35a988b7f0eb0071
Sone <sthaya@gmail.com>
2020-08-07 08:51:37 -0700

    completed ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,16 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    hash_table = {}
+    # loop through array
+    for i in range(length):
+        weight = weights[i]
+        # check if weight is in hash table
+        if weight in hash_table:
+            value = hash_table[weight]
+            return [i, value]
+        # finds the difference
+        difference = limit - weight
+        hash_table[difference] = i
 
     return None
